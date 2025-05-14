f504c1000edf5c0ff8bf138d4df3e0ba62fc8298
rogersruga <rogersruga.g@gmail.com>
2025-05-14 11:23:13 +0300

    Variables Lesson 1

--- variables.dart
+++ variables.dart
@@ -1,4 +1,48 @@
-void main(){
+void main() {
   //Variables
+
+  // <data type> <variable name> = <value>;
+
+  //int, double, string, bool (These are datatypes)
+
+  //Integer (int)
+
+  int firstValue = 19, secondValue = 5;
+
+  print(firstValue + secondValue);
+  print(firstValue - secondValue);
+
+
+  //String (string)
   
+  String firstWord = "Hello", secondWord = "World";
+
+  print(firstWord);
+
+
+  //Boolean (bool)
+
+  bool isBoy = true;
+  print(isBoy);
+
+
+  //Dynamic (dynamic) - It can take any vale ie: int, double, string, bool
+  //NOT RECOMMENDED IN MOST CASES
+
+  dynamic someValue = 10.5;
+  print(someValue);
+
+
+
+//INTERPOLATION 
+// If you want to add another variable to a string, you can use interpolation. 
+// You use '$previousVariable newVariable'
+
+String greeting = "Hello";
+print(greeting);
+
+greeting = '$greeting Yoooo';
+print(greeting);
+  
+
 }
