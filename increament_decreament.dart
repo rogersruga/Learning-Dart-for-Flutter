1176d7d825bbde7f068662bf1740ba41972f8e91
rogersruga <rogersruga.g@gmail.com>
2025-05-22 15:42:44 +0300

    increament & decreament

---
+++ increament_decreament.dart
@@ -0,0 +1,22 @@
+void main() {
+
+  // ++var - pre increment
+  // --var - pre decrement
+
+  // var++ - post increment
+  // var-- - post decrement
+
+  int age = 24;
+
+
+  //Post increment
+  print(age++); // This will print the value of age first and then increment therefore printing 24
+  print(age); // This will print the incremented value of age which is 25
+
+
+
+  //Pre increment
+  int age2 = 24;
+  print(++age2); // This will increment the value of age first and then print therefore printing 25
+  print(age2); // This will print the incremented value of age which is 25
+}
