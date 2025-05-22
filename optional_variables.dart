91e4f54acdebc6db5f6ed1e82eff70f8d2e49ddb
rogersruga <rogersruga.g@gmail.com>
2025-05-22 14:50:35 +0300

    Variables Lesson 2

---
+++ optional_variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Optional Variables
+  // String/int/bool  and null
+
+  String someValue = '2'; // this is an empty value not a null value
+  String? some = null; // this is a null value
+
+  print(someValue);
+  print(some);
+}
---
+++ variables_2.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Variables
+  // var/final/const variableName = value;
+
+  var someValue = "10";
+  final someValue2 = "10";
+  const someValue3 = "10";
+
+  print(someValue);
+  print(someValue2);
+  print(someValue3);
+}
